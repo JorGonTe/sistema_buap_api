70b66e01cf879d2137f0d89f9962a7ef81f5cfa9
Luis Yael Mendez <luisyaelms@gmail.com>
2024-11-07 16:25:56 -0600

    Edición de usuarios
    Se agrega el editar admin y maestros.

--- sistema_buap_api/urls.py
+++ sistema_buap_api/urls.py
@@ -22,18 +22,26 @@ from sistema_buap_api.views import maestros
 from sistema_buap_api.views import auth
 
 urlpatterns = [
-    #Version
+     #Version
         path('bootstrap/version', bootstrap.VersionView.as_view()),
     #Create Admin
         path('admin/', users.AdminView.as_view()),
     #Admin Data
         path('lista-admins/', users.AdminAll.as_view()),
+    #Edit Admin
+        path('admins-edit/', users.AdminsViewEdit.as_view()),
     #Create Alumno
         path('alumnos/', alumnos.AlumnosView.as_view()),
+    #Alumno Data
+        path('lista-alumnos/', alumnos.AlumnosAll.as_view()),
+    #Edit Alumno
+        #path('alumnos-edit/', alumnos.AlumnosViewEdit.as_view()),
     #Create Maestro
         path('maestros/', maestros.MaestrosView.as_view()),
     #Maestro Data
         path('lista-maestros/', maestros.MaestrosAll.as_view()),
+    #Edit Maestro
+        path('maestros-edit/', maestros.MaestrosViewEdit.as_view()),
     #Login
         path('token/', auth.CustomAuthToken.as_view()),
     #Logout
